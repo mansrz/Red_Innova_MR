2c8eb0cc9aacad48b7537f09fd0ed56eafe8f55c
Manuel Suárez <man.srz@gmail.com>
2015-02-01 20:36:25 -0500

    Add view login

--- web/views.py
+++ web/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden
+from django.contrib.auth import authenticate, login
+
+
+
+
+
+
+
+
+
+
+
+
+
+def login(request):
+    if request.method == 'POST':
+        try:
+            username = request.GET['username']
+            password = request.GET['password']
+        except:
+            return HttpResponse('Malos Parametros ')
+        user = authenticate(username=username, password=password)
+        if user is not None: 
+            login(request, user)
+            response_content = {
+                'username': user.username,
+
+            }
+        else:
+            return HttpResponseBadRequest('Usario o contraseña incorrecto')
+            
+
 
 # Create your views here.
+#login
+#formularios de registro
+#profile
+# 

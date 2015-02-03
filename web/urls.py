dfe71024d87b1940948720bd781d82dbe9993aaf
Ramón Carrillo <racarrillo91@gmail.com>
2015-02-03 04:46:11 -0500

    Serve static pages ending with .html
    Capture all urls ending with .html and serve the template with the same
    name as the requested url

--- web/urls.py
+++ web/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home),
+    url(r'^(.*\.html)$', views.static_page),
 )
--- web/views.py
+++ web/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def static_page(request, page):
+    return render(request, page)

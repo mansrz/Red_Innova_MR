1f9c7c895525a7cb5b38cff4748d3f8cb4bea029
Manuel Suárez <man.srz@gmail.com>
2015-02-06 11:08:53 -0500

    Add forms

--- web/forms.py
+++ web/forms.py
@@ -4,6 +4,24 @@ from web.models import Persona, Institucion
 
 class InstitucionForm(forms.ModelForm)
     class Meta:
-        pass
+        model = Institucion
+        fields = ['nombre_abreviado','ciudad','pais',\
+                 'sitio_web','telefono','email','estado'\
+                 ,'descripcion_general','logo','mision'\
+                 ,'nombre_aplicante','nombre_encargado'\
+                 ,'recursos_ofrece']
+ 
+class PersonaForm(forms.ModelForm)
+    class Meta:
+        model = Institucion
+        fields = ['name','ciudad','pais',\
+                 'sitio_web','telefono','email','estado'\
+                 ,'descripcion_general','foto','cargo'\
+                 ,'institucion','cargo','actividad'\
+                 ,'areas_interes','identificacion']
+ 
+
+
+        
 
 
